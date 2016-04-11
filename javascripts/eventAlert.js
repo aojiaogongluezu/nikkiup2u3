2b9334fcb6ecb547aef9d3fa80437f9df50bbdaf
reanrean <reanrean@users.noreply.github.com>
2016-04-12 03:58:44 +0800

    fit for iphone...

--- javascripts/eventAlert.js
+++ javascripts/eventAlert.js
@@ -4,53 +4,44 @@ window.onload = function(){
 };
 
 function event_alert(){
-	var out='';
+	var ret=[];
 	//calc for contest
-	var date=new Date(new Date().getTime() + 8*60*60*1000 + new Date().getTimezoneOffset()*60000);
+	var d=new Date();
+	var date=new Date(d.getTime() + 8*60*60*1000 + d.getTimezoneOffset()*60000);
 	if((date.getDay()==1||date.getDay()==5)&&date.getHours()>5){
 		var h = 23-date.getHours() +5;
 		var m = 59-date.getMinutes();
-		out+='搭配评选赛&emsp;'+h+'时'+m+'分后结算<br>';
+		ret.push([1,'搭配评选赛&emsp;'+h+'时'+m+'分后结算']);
 	}
 	else if((date.getDay()==2||date.getDay()==6)&&date.getHours()<5){
 		var h = 4-date.getHours();
 		var m = 59-date.getMinutes();
-		out+='搭配评选赛&emsp;'+h+'时'+m+'分后结算<br>';
+		ret.push([1,'搭配评选赛&emsp;'+h+'时'+m+'分后结算']);
 	}
 	
-	for (var i in eventList){//event ending in 24hrs
-		var res=getEventTime(eventList[i]);
-		if(res[0]==1&&res[1]<1){
-			out+=eventList[i][0]+'&emsp;<span style="color:red">'+res[2]+'时'+res[3]+'分后结束</span><br>';
+	var time_now=d.getTime();
+	for(var i in eventList){
+		var time_start=new Date(eventList[i][1]+":00 GMT+0800").getTime();
+		var time_end=new Date(eventList[i][2]+":00 GMT+0800").getTime();
+		if(time_now<time_start) {
+			var time_d = Math.floor((time_start-time_now)/1000/60/60/24);
+			var time_h = Math.floor((time_start-time_now)/1000/60/60)%24;
+			var time_m = Math.floor((time_start-time_now)/1000/60)%60;
+			if(time_d<1) {ret.push([2,eventList[i][0]+'&emsp;'+time_h+'时'+time_m+'分后开启']);}
+		}
+		else if(time_now<time_end) {
+			var time_d = Math.floor((time_end-time_now)/1000/60/60/24);
+			var time_h = Math.floor((time_end-time_now)/1000/60/60)%24;
+			var time_m = Math.floor((time_end-time_now)/1000/60)%60;
+			if(time_d<1) {ret.push([0,eventList[i][0]+'&emsp;<span style="color:red">'+time_h+'时'+time_m+'分后结束</span>']);}
+			else {ret.push([1,eventList[i][0]+'&emsp;'+time_d+'天'+time_h+'时'+time_m+'分后结束']);}
 		}
 	}
-	for (var i in eventList){//event ending after 24 hrs
-		var res=getEventTime(eventList[i]);
-		if(res[0]==1&&res[1]>=1){
-			out+=eventList[i][0]+'&emsp;'+res[1]+'天'+res[2]+'时'+res[3]+'分后结束<br>';
-		}
-	}
-	for (var i in eventList){//event starting in future
-		var res=getEventTime(eventList[i]);
-		if(res[0]==0&&res[1]<1){//event start in 24 hrs
-			out+=eventList[i][0]+'&emsp;'+res[2]+'时'+res[3]+'分后开启<br>';
-		}
-	}
-	if(out){
+	
+	if(ret.length){
 		document.getElementById('event_alert').innerHTML='<table width="100%"><tr style="display:none"><td></td></tr><tr><td id="event_alert_c" style="text-align:left"></td></tr></table><br>';
-		document.getElementById('event_alert_c').innerHTML=/*'活动提醒：<br>'+*/out;
+		for(j=0;j<3;j++){ for(i=0;i<ret.length;i++){
+			if(ret[i][0]==j) {document.getElementById('event_alert_c').innerHTML+=ret[i][1]+'<br>';}
+		}}
 	}
-}
-
-function getEventTime(eventListItem){
-	var time_start=new Date(eventListItem[1]+" GMT+0800").getTime();
-	var time_end=new Date(eventListItem[2]+" GMT+0800").getTime();
-	var time_now=new Date().getTime();
-	if(time_now<time_start) {var time_c=time_start; var ind=0;}
-	else if(time_now<time_end) {var time_c=time_end; var ind=1;}
-	else {return [2,0,0,0];}
-	var time_d = Math.floor((time_c-time_now)/1000/60/60/24);
-	var time_h = Math.floor((time_c-time_now)/1000/60/60)%24;
-	var time_m = Math.floor((time_c-time_now)/1000/60)%60;
-	return [ind,time_d,time_h,time_m];
 }
