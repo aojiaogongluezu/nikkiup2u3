01576be4c22c4953cf492fa40436a47b0836b129
reanrean <reanrean@users.noreply.github.com>
2016-05-12 20:11:02 +0800

    add prop analysis

--- html/3-DingPei/dp.js
+++ html/3-DingPei/dp.js
@@ -8,23 +8,39 @@ function hideTop(id){
 }
 function chgMode(mode){
 	var x=[];
-	if (mode=='l'){
-		x = document.getElementsByClassName('norm');
-		for (var i=0;i<x.length;i++) {x[i].style.display="none";}
-		x = document.getElementsByClassName('limit');
-		for (var i=0;i<x.length;i++) {x[i].style.display="inline";}
-	}else{
-		x = document.getElementsByClassName('norm');
-		for (var i=0;i<x.length;i++) {x[i].style.display="inline";}
-		x = document.getElementsByClassName('limit');
-		for (var i=0;i<x.length;i++) {x[i].style.display="none";}
+	switch(mode){
+		case 'n':
+			x = document.getElementsByClassName('norm');
+			for (var i=0;i<x.length;i++) {x[i].style.display="inline";}
+			x = document.getElementsByClassName('limit');
+			for (var i=0;i<x.length;i++) {x[i].style.display="none";}
+			x = document.getElementsByClassName('prop');
+			for (var i=0;i<x.length;i++) {x[i].style.display="none";}
+			break;
+		case 'l':
+			x = document.getElementsByClassName('limit');
+			for (var i=0;i<x.length;i++) {x[i].style.display="inline";}
+			x = document.getElementsByClassName('norm');
+			for (var i=0;i<x.length;i++) {x[i].style.display="none";}
+			x = document.getElementsByClassName('prop');
+			for (var i=0;i<x.length;i++) {x[i].style.display="none";}
+			break;
+		case 'p':
+			x = document.getElementsByClassName('prop');
+			for (var i=0;i<x.length;i++) {x[i].style.display="inline";}
+			x = document.getElementsByClassName('norm');
+			for (var i=0;i<x.length;i++) {x[i].style.display="none";}
+			x = document.getElementsByClassName('limit');
+			for (var i=0;i<x.length;i++) {x[i].style.display="none";}
+			break;
 	}
 }
 window.onload = function(){
 	var radio_text='<form action="">';
 	radio_text+='<input type="radio" name="limit" id="limitn" value="n" checked onclick="chgMode(this.value)"><label for="limitn">标准权重</label>';
-	radio_text+='<input type="radio" name="limit" id="limitl" value="l" onclick="chgMode(this.value)"><label for="limitl">极限权重</label><br>';
-	radio_text+='</form>';
+	radio_text+='<input type="radio" name="limit" id="limitl" value="l" onclick="chgMode(this.value)"><label for="limitl">极限权重</label>';
+	if(document.getElementsByClassName('prop').length>0) radio_text+='<input type="radio" name="limit" id="limitp" value="p" onclick="chgMode(this.value)"><label for="limitp">属性分析</label>';
+	radio_text+='<br></form>';
 	if(document.getElementById("radio")) {document.getElementById("radio").innerHTML = radio_text;}
 	
 	var elts = document.getElementsByTagName('a');
