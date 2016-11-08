3efb0e536dc33c82aa45d76f4bc1bfe04f70ae7a
reanrean <rinnerean@yahoo.co.jp>
2016-11-09 01:14:41 +0800

    你妹的4.0属性提升

--- data/levels-hs.js
+++ data/levels-hs.js
@@ -228,7 +228,7 @@ var tasksAdd={
 '关卡: 15-5':['cool','active'],
 '关卡: 15-6':['simple','active'],
 '关卡: 15-7':['active','cute'],
-'关卡: 15-8':['simple','cute'],
+'关卡: 15-8':['active','cute'],
 '关卡: 15-9':['active','simple'],
 '关卡: 15-支1':['simple','active'],
 '关卡: 15-支2':['simple','active'],
